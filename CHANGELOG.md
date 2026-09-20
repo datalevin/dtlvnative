27cfdabdd3a2d978018f7be20dfd25777bd29a69
Huahai Yang <huahai.yang@gmail.com>
2026-09-19 17:18:17 -0700

    Version 1.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 1.1.4
+### Improved
+- update DLMDB to speedup writes by using generation tag for prefix cache
+
 ## 1.1.3
 ### Improved
 - make JVM and rust version consistent
--- windows-x86_64/project.clj
+++ windows-x86_64/project.clj
@@ -1,4 +1,4 @@
-(def version "1.1.3")
+(def version "1.1.4")
 
 (defproject org.clojars.huahaiy/dtlvnative-windows-x86_64 version
   :description "Native dependency of Datalevin database on Windows"
